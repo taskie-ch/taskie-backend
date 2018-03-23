ecfe041e9ea0288c3f3c5db52f73133bfd1ea824
Simon Anliker <anliksim@users.noreply.github.com>
2018-03-23 23:20:37 +0100

    Basic rest service
      - hello world resource
      - health check example
      - taskie banner

---
+++ src/main/java/com/taskie/TaskieApplication.java
@@ -0,0 +1,42 @@
+package com.taskie;
+
+import com.taskie.health.TemplateHealthCheck;
+import com.taskie.resources.HelloWorldResource;
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class TaskieApplication extends Application<TaskieConfiguration> {
+
+    public static void main(final String[] args) throws Exception {
+        new TaskieApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "Taskie Server";
+    }
+
+    @Override
+    public void initialize(final Bootstrap<TaskieConfiguration> bootstrap) {
+        // TODO: application initialization
+    }
+
+    @Override
+    public void run(final TaskieConfiguration configuration,
+                    final Environment environment) {
+
+
+        final HelloWorldResource resource = new HelloWorldResource(
+                configuration.getTemplate(),
+                configuration.getDefaultName()
+        );
+
+        final TemplateHealthCheck healthCheck =
+                new TemplateHealthCheck(configuration.getTemplate());
+        environment.healthChecks().register("template", healthCheck);
+
+        environment.jersey().register(resource);
+    }
+
+}

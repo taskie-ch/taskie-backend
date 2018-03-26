7068ca22e34b9fb12c5a34f3a4baf1a35b4dbbd6
Simon Anliker <anliksim@users.noreply.github.com>
2018-03-26 23:19:41 +0200

    Setup API documentation with swagger (#3)
      - add basic configuration
      - document hello-world resource
      - document task resource
      - raise dropwizard version to 1.3

--- src/main/java/com/taskie/TaskieApplication.java
+++ src/main/java/com/taskie/TaskieApplication.java
@@ -7,6 +7,8 @@ import com.taskie.resources.TaskResource;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import io.federecio.dropwizard.swagger.SwaggerBundle;
+import io.federecio.dropwizard.swagger.SwaggerBundleConfiguration;
 
 public class TaskieApplication extends Application<TaskieConfiguration> {
 
@@ -21,7 +23,12 @@ public class TaskieApplication extends Application<TaskieConfiguration> {
 
     @Override
     public void initialize(final Bootstrap<TaskieConfiguration> bootstrap) {
-        // TODO: application initialization
+        bootstrap.addBundle(new SwaggerBundle<TaskieConfiguration>() {
+            @Override
+            protected SwaggerBundleConfiguration getSwaggerBundleConfiguration(final TaskieConfiguration configuration) {
+                return configuration.swaggerBundleConfiguration;
+            }
+        });
     }
 
     @Override
--- src/main/java/com/taskie/resources/HelloWorldResource.java
+++ src/main/java/com/taskie/resources/HelloWorldResource.java
@@ -2,6 +2,8 @@ package com.taskie.resources;
 
 import com.codahale.metrics.annotation.Timed;
 import com.taskie.api.Saying;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -12,6 +14,7 @@ import java.util.concurrent.atomic.AtomicLong;
 import java.util.Optional;
 
 @Path("/hello-world")
+@Api(value = "hello-world")
 @Produces(MediaType.APPLICATION_JSON)
 public class HelloWorldResource {
 
@@ -27,6 +30,8 @@ public class HelloWorldResource {
 
     @GET
     @Timed
+    @ApiOperation(value = "Says hello",
+            notes = "A name can be provided to be more specific")
     public Saying sayHello(@QueryParam("name") Optional<String> name) {
         final String value = String.format(template, name.orElse(defaultName));
         return new Saying(counter.incrementAndGet(), value);

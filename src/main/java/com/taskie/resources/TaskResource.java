b37a751a5e9b9749306f2d167b474b1d1096ac4f
Simon Anliker <anliksim@users.noreply.github.com>
2018-03-27 00:43:39 +0200

    Optimize imports of TaskResource

--- src/main/java/com/taskie/resources/TaskResource.java
+++ src/main/java/com/taskie/resources/TaskResource.java
@@ -1,16 +1,16 @@
 package com.taskie.resources;
 
 import com.codahale.metrics.annotation.Timed;
-import com.taskie.api.Saying;
 import com.taskie.core.Task;
 import com.taskie.db.TaskDao;
 import io.dropwizard.jersey.params.LongParam;
 
-import javax.ws.rs.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.Collection;
-import java.util.Optional;
-import java.util.concurrent.atomic.AtomicLong;
 
 @Path("/task")
 @Produces(MediaType.APPLICATION_JSON)
